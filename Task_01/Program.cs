46771d083d2534d83569e1cdaad5d4efbd522876
onicolli <onicolli>
2023-01-30 23:26:41 +0600

    Do Task_02

---
+++ Task_01/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+int Num = new Random().Next(10, 100);
+int A1 = Num / 10;
+int A2 = Num % 10;
+int Max = A1;
+if (Max < A2) {
+    Max = A2;
+}
+Console.WriteLine($"Максимальное цифра в числе: {Num} -> {Max}");
---
+++ Task_02/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+int Num = new Random().Next(100,1000);
+int s = Num;
+int e = Num;
+s = Num % 10;
+e = Num / 100;
+
+Console.WriteLine($"Из числа {Num} получилось число {e}{s}");

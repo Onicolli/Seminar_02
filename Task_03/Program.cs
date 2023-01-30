f5924b5cd39f421dd193255c3059ec138c3ce3ea
onicolli <onicolli>
2023-01-30 23:49:16 +0600

    Do Task_03

---
+++ Task_03/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+int n1 = new Random().Next(1, 1000);
+int n2 = new Random().Next(1, 1000);
+
+if(n1%n2 == 0) 
+{
+    Console.WriteLine($"{n1}, {n2} -> кратно");
+}
+else
+{
+    int n = n1%n2 ;
+    Console.WriteLine ($"{n1}, {n2} -> не кратно, остаток {n}");
+}

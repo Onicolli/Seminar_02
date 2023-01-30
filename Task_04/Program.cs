6f7f4414333d024f50bc1a6191ee61f12d581da8
onicolli <onicolli>
2023-01-31 00:10:03 +0600

    Do Task_04

---
+++ Task_04/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int Num = int.Parse (Console.ReadLine()); 
+
+if(Num % 7 == 0 && Num % 23 == 0)
+{
+    Console.WriteLine($"{Num} -> ДА");
+}
+else {
+    Console.WriteLine($"{Num} -> НЕТ");
+}
+

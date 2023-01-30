d00488879adfefb76840732e197e3bd402bd7a06
onicolli <onicolli>
2023-01-30 23:30:13 +0600

    Rewrite Task_02

--- Task_02/Program.cs
+++ Task_02/Program.cs
@@ -1,8 +1,5 @@
 ﻿Console.Clear();
 int Num = new Random().Next(100,1000);
-int s = Num;
-int e = Num;
-s = Num % 10;
-e = Num / 100;
-
+int s = Num % 10;
+int e = Num / 100;
 Console.WriteLine($"Из числа {Num} получилось число {e}{s}");

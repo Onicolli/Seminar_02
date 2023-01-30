5884a9f712ce9bea92a6ea1d5b6fb9a4a2a78106
onicolli <onicolli>
2023-01-31 00:19:16 +0600

    Do Task_05

---
+++ Task_05/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Enter 1st number: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter 2nd number: ");
+int b = int.Parse(Console.ReadLine());
+if (a*a == b || b*b == a)
+{
+    Console.WriteLine($"{a},{b} -> да");
+}
+else
+{
+    Console.WriteLine($"{a},{b} -> нет");
+}

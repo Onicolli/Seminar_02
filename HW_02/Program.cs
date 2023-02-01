bc8d4e9f7f5afe0f818784f9a406dbb1c1227274
onicolli <onicolli>
2023-02-01 20:39:37 +0600

    Do HomeWork_02

---
+++ HW_02/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.WriteLine("Введите любое число: ");
+int Num = int.Parse(Console.ReadLine());
+if (Num > 99) { Console.WriteLine((Num / 100) % 10); }
+else { Console.WriteLine("В числе не 3 числа"); }

f5a475eba8be56bd3909d690f0ef41708cd4534e
onicolli <onicolli>
2023-02-04 13:04:33 +0600

    Do HomeWork_03

---
+++ HW_03/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int Num = int.Parse(Console.ReadLine()!);
+if ((Num > 7) || (Num < 1)) {
+    Console.WriteLine("Введите число от 1 - 7");
+}
+if (Num > 5) {
+    Console.WriteLine("Да");
+}
+if (Num <= 5) {
+    Console.WriteLine("Нет");
+}
+
